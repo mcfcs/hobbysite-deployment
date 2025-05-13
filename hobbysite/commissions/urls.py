b81ead2ccc5827820e65ba3cd9d1e9c10c5709f5
mcfc <161129995+mcfcs@users.noreply.github.com>
2025-05-13 20:52:54 +0800

    Delete hobbysite directory

--- hobbysite/commissions/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CommentListView, CommentDetailView
-
-urlpatterns = [
-    path('list/', CommentListView.as_view(), name = 'comment_list'), 
-    path('detail/<int:pk>/', CommentDetailView.as_view(), name = 'comment_detail'),
-]
-
-app_name = "commissions"
